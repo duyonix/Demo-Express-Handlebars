c891278229deac3ef34b25f57c1829b7325b2dc1
ydam <ydamk15nbk@gmail.com>
2022-05-18 00:02:47 +0700

    yne

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const expressHbs = require("express-handlebars");
+const expressHbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -8,55 +8,64 @@ const hbs = expressHbs.create({
     extname: 'hbs',
     defaultLayout: 'main',
     layoutsDir: path.join(__dirname, 'views/layouts'),
-    partialsDir: path.join(__dirname, 'views/partials')
+    partialsDir: path.join(__dirname, 'views/partials'),
 });
 
-app.engine("hbs", hbs.engine)
-app.set("view engine", "hbs")
-app.set("views", path.join(__dirname, "views"))
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-    res.render("index", {
-        author: "Nhom06"
-    })
-})
+app.get('/', (req, res) => {
+    res.render('index', {
+        author: 'Nhom06',
+    });
+});
 
-app.get("/task1", (req, res) => {
-    res.render("task1", {
-        author: "MSSV - Ho ten"
-    })
-})
+app.get('/task1', (req, res) => {
+    res.render('task1', {
+        author: 'MSSV - Ho ten',
+    });
+});
 
-app.get("/task2", (req, res) => {
-    res.render("task2", {
-        author: "MSSV - Ho ten"
-    })
-})
+app.get('/task2', (req, res) => {
+    let salary = parseFloat(req.query.salary || 0);
+    res.locals.jars = [
+        (salary * 55) / 100,
+        (salary * 10) / 100,
+        (salary * 5) / 100,
+        (salary * 10) / 100,
+        (salary * 10) / 100,
+        (salary * 10) / 100,
+    ];
+    res.render('task2', {
+        author: '19120160 - Đàm Thị Xuân Ý',
+    });
+});
 
-app.get("/task3", (req, res) => {
-    res.render("task3", {
-        author: "MSSV - Ho ten"
-    })
-})
+app.get('/task3', (req, res) => {
+    res.render('task3', {
+        author: 'MSSV - Ho ten',
+    });
+});
 
-app.get("/task4", (req, res) => {
-    res.render("task4", {
-        author: "MSSV - Ho ten"
-    })
-})
+app.get('/task4', (req, res) => {
+    res.render('task4', {
+        author: 'MSSV - Ho ten',
+    });
+});
 
-app.get("/task4/:name", (req, res) => {
-    res.render("task4-details", {
-        author: "MSSV - Ho ten"
-    })
-})
+app.get('/task4/:name', (req, res) => {
+    res.render('task4-details', {
+        author: 'MSSV - Ho ten',
+    });
+});
 
-app.set("port", process.env.PORT || 3000)
-app.listen(app.get("port"), () => {
-    console.log("Server is running on port " + app.get("port"))
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+    console.log('Server is running on port ' + app.get('port'));
 });
